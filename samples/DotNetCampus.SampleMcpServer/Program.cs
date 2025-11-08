13c7efb07d3d722eef72a84187d1f1a163bd3a81
walterlv <xblero@gmail.com>
2025-11-08 11:44:41 +0800

    添加一个最最基本的 http 监听的示例

---
+++ samples/DotNetCampus.SampleMcpServer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetCampus.ModelContextProtocol.Transports;
+
+var httpTransport = new HttpTransport("http://localhost:5942/");
+await httpTransport.StartAsync(async d =>
+{
+    return d;
+});
