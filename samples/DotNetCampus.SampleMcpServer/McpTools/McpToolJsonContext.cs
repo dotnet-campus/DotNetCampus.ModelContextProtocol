b79940824fd9e9359246e67c56ae8c1a935591bf
walterlv <xblero@gmail.com>
2025-11-11 11:27:29 +0800

    删除不需要的 JSON 类型定义

--- samples/DotNetCampus.SampleMcpServer/McpTools/McpToolJsonContext.cs
+++ samples/DotNetCampus.SampleMcpServer/McpTools/McpToolJsonContext.cs
@@ -1,12 +1,7 @@
 ﻿using System.Text.Json.Serialization;
-using DotNetCampus.ModelContextProtocol.Protocol;
 
 namespace DotNetCampus.SampleMcpServer.McpTools;
 
-[JsonSerializable(typeof(int))]
-[JsonSerializable(typeof(int?))]
-[JsonSerializable(typeof(string))]
-[JsonSerializable(typeof(CallToolResult))]
 [JsonSerializable(typeof(EchoExtraData))]
 [JsonSerializable(typeof(EchoOptions))]
 [JsonSourceGenerationOptions(
