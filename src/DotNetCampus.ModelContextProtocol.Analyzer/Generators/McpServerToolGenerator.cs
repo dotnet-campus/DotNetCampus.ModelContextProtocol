6472683b611bc32ee5132cd28e16d14e65c16f08
walterlv <xblero@gmail.com>
2025-11-13 11:55:16 +0800

    暂时先去掉异常处理

--- src/DotNetCampus.ModelContextProtocol.Analyzer/Generators/McpServerToolGenerator.cs
+++ src/DotNetCampus.ModelContextProtocol.Analyzer/Generators/McpServerToolGenerator.cs
@@ -32,27 +32,20 @@ public class McpServerToolGenerator : IIncrementalGenerator
 
     private void Execute(SourceProductionContext context, McpServerToolGeneratingModel model)
     {
-        try
-        {
-            using var builder = new SourceTextBuilder(model.Namespace)
-                .Using("System.Text.Json")
-                .AddTypeDeclaration(
-                    $"{model.GetGetAccessModifier()} sealed class {model.GetBridgeTypeName()}({G.Func}<{model.ContainingType.ToUsingString()}> targetFactory)",
-                    t => t.AddBaseTypes(G.IMcpServerTool)
-                        .WithSummaryComment($"为 <see cref=\"{model.ContainingType.ToUsingString()}.{model.Method.Name}\"/> 方法生成的 MCP 服务器工具桥接类。")
-                        .AddRawText($"private readonly {G.Func}<{model.ContainingType.ToUsingString()}> _targetFactory = targetFactory;")
-                        .AddRawText($"private {model.ContainingType.ToUsingString()} Target => _targetFactory();")
-                        .AddRawText($"/// <inheritdoc />\npublic string ToolName {{ get; }} = \"{model.Name}\";")
-                        .AddGetToolDefinitionMethod(model)
-                        .AddGetInputSchemaMethod(model)
-                        .AddCallToolMethod(model)
-                );
-            var code = builder.ToString();
-            context.AddSource($"{model.Namespace}/{model.ContainingType.ToDeclarationNestedDisplayString()}.{model.Method.Name}.cs", code);
-        }
-        catch (Exception ex)
-        {
-            throw new NotImplementedException(ex.ToString().Replace("\n", " ").Replace("\r", ""));
-        }
+        using var builder = new SourceTextBuilder(model.Namespace)
+            .Using("System.Text.Json")
+            .AddTypeDeclaration(
+                $"{model.GetGetAccessModifier()} sealed class {model.GetBridgeTypeName()}({G.Func}<{model.ContainingType.ToUsingString()}> targetFactory)",
+                t => t.AddBaseTypes(G.IMcpServerTool)
+                    .WithSummaryComment($"为 <see cref=\"{model.ContainingType.ToUsingString()}.{model.Method.Name}\"/> 方法生成的 MCP 服务器工具桥接类。")
+                    .AddRawText($"private readonly {G.Func}<{model.ContainingType.ToUsingString()}> _targetFactory = targetFactory;")
+                    .AddRawText($"private {model.ContainingType.ToUsingString()} Target => _targetFactory();")
+                    .AddRawText($"/// <inheritdoc />\npublic string ToolName {{ get; }} = \"{model.Name}\";")
+                    .AddGetToolDefinitionMethod(model)
+                    .AddGetInputSchemaMethod(model)
+                    .AddCallToolMethod(model)
+            );
+        var code = builder.ToString();
+        context.AddSource($"{model.Namespace}/{model.ContainingType.ToDeclarationNestedDisplayString()}.{model.Method.Name}.cs", code);
     }
 }
