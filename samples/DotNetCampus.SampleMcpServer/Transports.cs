f665f879680d1e8c2fb52f2e1b97a83516e3ad7d
walterlv <xblero@gmail.com>
2026-01-27 09:54:29 +0800

    加入源生成器所需的标记类型

---
+++ samples/DotNetCampus.SampleMcpServer/Transports.cs
@@ -0,0 +1,6 @@
+﻿using DotNetCampus.ModelContextProtocol.CompilerServices;
+
+namespace DotNetCampus.SampleMcpServer;
+
+[GenerateMcpTransport(McpTransportPackageId.TouchSocketHttp, McpSide.Server)]
+public partial class TouchSocketHttpServerTransport;
---
+++ src/DotNetCampus.ModelContextProtocol/CompilerServices/McpSide.cs
@@ -0,0 +1,17 @@
+namespace DotNetCampus.ModelContextProtocol.CompilerServices;
+
+/// <summary>
+/// 表示 MCP 协议的端。
+/// </summary>
+public enum McpSide
+{
+    /// <summary>
+    /// 表示 MCP 协议的服务端。
+    /// </summary>
+    Server,
+
+    /// <summary>
+    /// 表示 MCP 协议的客户端。
+    /// </summary>
+    Client,
+}
