a54e1e50c2ee361a31f4e8dc4ca5f93f20ae2591
walterlv <xblero@gmail.com>
2026-01-27 16:23:22 +0800

    添加 TouchSocket 服务端传输层

---
+++ src/DotNetCampus.ModelContextProtocol.TouchSocket/Transports/TouchSocket/TouchSocketServerTransport.cs
@@ -0,0 +1,16 @@
+﻿using DotNetCampus.ModelContextProtocol.Transports;
+
+namespace DotNetCampus.ModelContextProtocol.TouchSocket.Transports.TouchSocket;
+
+public class TouchSocketServerTransport : IServerTransport
+{
+    public async Task<Task> StartAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
