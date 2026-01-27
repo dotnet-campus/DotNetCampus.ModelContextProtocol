182d8389aa48f09ec749e859d03fa2f068a676ba
walterlv <xblero@gmail.com>
2026-01-27 10:55:47 +0800

    计划生成 MCP 传输层

---
+++ src/DotNetCampus.ModelContextProtocol.Analyzer/Generators/GenerateMcpTransportGenerator.cs
@@ -0,0 +1,23 @@
+﻿using DotNetCampus.ModelContextProtocol.CompilerServices;
+using DotNetCampus.ModelContextProtocol.Generators.ModelProviders;
+using DotNetCampus.ModelContextProtocol.Generators.Models;
+using Microsoft.CodeAnalysis;
+
+namespace DotNetCampus.ModelContextProtocol.Generators;
+
+[Generator(LanguageNames.CSharp)]
+public class GenerateMcpTransportGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var serverTransportProvider = context.SyntaxProvider.SelectMcpTransport<GenerateMcpServerTransportAttribute>();
+        var clientTransportProvider = context.SyntaxProvider.SelectMcpTransport<GenerateMcpClientTransportAttribute>();
+
+        context.RegisterSourceOutput(serverTransportProvider, GenerateTransport);
+        context.RegisterSourceOutput(clientTransportProvider, GenerateTransport);
+    }
+
+    private void GenerateTransport(SourceProductionContext context, McpTransportGeneratingModel model)
+    {
+    }
+}
